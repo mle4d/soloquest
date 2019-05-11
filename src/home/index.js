81ca3fb27fce37081ad03d2100d4ae46a71df149
emily <mle4d@me.com>
2019-05-11 15:38:36 -0700

    fixes

--- src/home/index.js
+++ src/home/index.js
@@ -8,6 +8,6 @@ userSignUp.addEventListener('submit', (event) => {
     const formData = new FormData(userSignUp);
     const user = makeUser(formData);
     // should signUp be saveUser
-    api.signUp(user);
+    api.saveUser(user);
     window.location = './src/map/map.html';
 });
